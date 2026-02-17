4de8fb88101c3533d34865ca75798533e16f8258
Samuel Correia <thenenouno1@gmail.com>
2026-02-17 15:58:48 -0300

    NPSV-38 feat: Criar projeto Spring Boot com Java 21

---
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/NpsvGestorTintasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.senai.npsv_gestor_tintas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsvGestorTintasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsvGestorTintasBackendApplication.class, args);
+	}
+
+}
