bbe0eed3a8742e93a72597c5da1bbbb7ca23a5c6
Samuel Correia <thenenouno1@gmail.com>
2026-02-18 11:08:23 -0300

    NPSV-44 feat: Definir endpoints por recurso

--- src/main/java/com/senai/npsv_gestor_tintas_backend/interface_ui/controller/ProdutoController.java
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/interface_ui/controller/ProdutoController.java
@@ -1,4 +1,11 @@
 package com.senai.npsv_gestor_tintas_backend.interface_ui.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produtos/")
+@RequiredArgsConstructor
 public class ProdutoController {
 }
--- src/main/java/com/senai/npsv_gestor_tintas_backend/interface_ui/controller/UsuarioController.java
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/interface_ui/controller/UsuarioController.java
@@ -1,4 +1,15 @@
 package com.senai.npsv_gestor_tintas_backend.interface_ui.controller;
 
+import com.senai.npsv_gestor_tintas_backend.application.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios/")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService service;
+
+
 }
