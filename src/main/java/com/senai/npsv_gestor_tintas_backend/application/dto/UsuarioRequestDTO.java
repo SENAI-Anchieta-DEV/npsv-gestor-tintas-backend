7289c55a236da1a16b394575562a159b94c6dbbb
Samuel Correia <thenenouno1@gmail.com>
2026-02-26 19:29:56 -0300

    NPSV-48 feat(usuario): adiciona validações ao DTO e métodos do controlador

--- src/main/java/com/senai/npsv_gestor_tintas_backend/application/dto/UsuarioRequestDTO.java
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/application/dto/UsuarioRequestDTO.java
@@ -2,11 +2,22 @@ package com.senai.npsv_gestor_tintas_backend.application.dto;
 
 import com.senai.npsv_gestor_tintas_backend.domain.entity.Usuario;
 import com.senai.npsv_gestor_tintas_backend.domain.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UsuarioRequestDTO(
+        @NotBlank(message = "O nome não pode estar em branco.")
         String nome,
+
+        @NotBlank(message = "O e-mail não pode estar em branco.")
+        @Email(message = "O formato do e-mail é inválido.")
         String email,
+
+        @NotBlank(message = "A senha é obrigatória.")
         String senha,
+
+        @NotNull(message = "A role (ADMIN, COLORISTA ou VENDEDOR) deve ser informada.")
         Role role,
         boolean ativo
 ) {
