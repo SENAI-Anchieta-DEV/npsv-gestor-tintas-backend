ca5438b25bf0353a0c748afb605516e07e761c99
Samuel Correia <thenenouno1@gmail.com>
2026-02-26 21:16:24 -0300

    NPSV-48 feat(auth): adiciona validações ao DTO e ao controller de login

--- src/main/java/com/senai/npsv_gestor_tintas_backend/interface_ui/controller/AuthController.java
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/interface_ui/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.senai.npsv_gestor_tintas_backend.interface_ui.controller;
 
 import com.senai.npsv_gestor_tintas_backend.application.dto.AuthDTO;
 import com.senai.npsv_gestor_tintas_backend.application.service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class AuthController {
     private final AuthService auth;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthDTO.TokenResponse> login(@RequestBody AuthDTO.LoginRequest req) {
+    public ResponseEntity<AuthDTO.TokenResponse> login(@Valid @RequestBody AuthDTO.LoginRequest req) {
         log.info("Recebendo requisição de login para o email: {}", req.email());
         String token = auth.login(req);
         return ResponseEntity.ok(new AuthDTO.TokenResponse(token));
