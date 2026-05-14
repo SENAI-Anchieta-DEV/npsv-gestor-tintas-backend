8b1d727bdeabe3a5e9604215aa248985de35c14b
Samuel Correia <thenenouno1@gmail.com>
2026-05-14 11:08:50 -0300

    NPSV-318 feat(pedido): implementa controlador e serviços para gerenciamento de pedidos

--- src/main/java/com/senai/npsv_gestor_tintas_backend/application/dto/ItemPedidoRequestDTO.java
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/application/dto/ItemPedidoRequestDTO.java
@@ -1,5 +1,8 @@
 package com.senai.npsv_gestor_tintas_backend.application.dto;
 
+import com.senai.npsv_gestor_tintas_backend.domain.entity.ItemPedido;
+import com.senai.npsv_gestor_tintas_backend.domain.entity.Pedido;
+import com.senai.npsv_gestor_tintas_backend.domain.entity.Produto;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -19,4 +22,12 @@ public record ItemPedidoRequestDTO(
         @DecimalMin(value = "0.0", inclusive = false, message = "O preço unitário deve ser maior que zero.")
         BigDecimal precoUnitario
 ) {
+        public ItemPedido toEntity(Pedido pedido, Produto produto) {
+                return ItemPedido.builder()
+                        .pedido(pedido)
+                        .produto(produto)
+                        .quantidade(this.quantidade)
+                        .precoUnitario(this.precoUnitario)
+                        .build();
+        }
 }
--- src/main/java/com/senai/npsv_gestor_tintas_backend/application/dto/PedidoRequestDTO.java
+++ src/main/java/com/senai/npsv_gestor_tintas_backend/application/dto/PedidoRequestDTO.java
@@ -1,11 +1,17 @@
 package com.senai.npsv_gestor_tintas_backend.application.dto;
 
+import com.senai.npsv_gestor_tintas_backend.domain.entity.Fornecedor;
+import com.senai.npsv_gestor_tintas_backend.domain.entity.Pedido;
+import com.senai.npsv_gestor_tintas_backend.domain.entity.Usuario;
+import com.senai.npsv_gestor_tintas_backend.domain.enums.StatusPedido;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public record PedidoRequestDTO(
@@ -19,4 +25,15 @@ public record PedidoRequestDTO(
         @NotEmpty(message = "O pedido deve conter pelo menos um item.")
         List<@NotNull @Valid ItemPedidoRequestDTO> itens
 ) {
+        public Pedido toEntity(Usuario admin, Fornecedor fornecedor) {
+                return Pedido.builder()
+                        .fornecedor(fornecedor)
+                        .admin(admin)
+                        .status(StatusPedido.PENDENTE)
+                        .dataPedido(LocalDateTime.now())
+                        .dataPrevisaoEntrega(this.dataPrevisaoEntrega)
+                        .observacao(this.observacao)
+                        .itens(new ArrayList<>())
+                        .build();
+        }
 }
